40f6318a5077c81e8681903e518b4bf9d1cc2a29
i340771 <naresh.ganta@sap.com>
2026-04-06 20:14:10 +0530

    updating code

--- db/run.sh
+++ db/run.sh
@@ -4,3 +4,4 @@ set -x   # <-- prints every command
 mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} <schema.sql
 mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} <app-user.sql
 mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} <master-data.sql
+exec java -jar /app/shipping.jar
