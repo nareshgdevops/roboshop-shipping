69950131601b2ba1de9c717c4253e929b88d03ba
i340771 <naresh.ganta@sap.com>
2026-04-07 18:50:15 +0530

    updating code

--- db/run.sh
+++ db/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -x   # <-- prints every command
-mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} --ssl-mode=REQUIRED <schema.sql
-mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} --ssl-mode=REQUIRED <app-user.sql
-mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} --ssl-mode=REQUIRED <master-data.sql
+mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} --ssl <schema.sql
+mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} --ssl <app-user.sql
+mysql -h $DB_HOST -u${DB_USER} -p${DB_PASS} --ssl <master-data.sql
 exec java -jar /app/shipping.jar
